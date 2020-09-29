095be9cc7cce3b36e1e0a36532dc5cbec9e0acdb
Scott Mounce <scottmounce10@gmail.com>
2020-09-29 11:51:49 -0700

    have ALL data rendering in the browser

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,22 +1,36 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import ReviewList from './ReviewList.jsx';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {}
-  }
+function App() {
+  const [data, setData] = useState([])
+  const [numOfReviews, setNum] = useState(0)
+    
 
-
-
-
-
-  render() {
-    return (
-      <div>HELLO WORLD</div>
-
-
-    )
-  }
+  useEffect(() => {    
+    fetch('/api/allReviews')
+      .then((res) => {        
+        return res.json();
+      })
+      .then((results) => {
+        setNum(results.length)                          
+        setData(results)
+      })        
+      .catch(error => {
+        throw error;
+      })     
+    
+  }, [])
+  
+  
+  
+  return (
+    <div>
+      <div>{numOfReviews + ' reviews'}</div>
+      <ReviewList reviews={data}/>
+    </div>
+     
+  )
+  
 
 
 
---
+++ client/components/ReviewListEntry.jsx
@@ -0,0 +1,22 @@
+import React, {useState, useEffect}from 'react';
+import Moment from 'react-moment';
+
+const ReviewListEntry = props => {
+  
+  return (
+    <li>
+      <div className="review-author">{props.review.author}</div>
+      <div className="review-date"><Moment interval={1500} format="MMM D, YYYY" withTitle>{props.review.date}</Moment></div>
+      <div className="stars">{props.review.stars}</div>
+      <div className="purchased-item">{props.review.purchasedItem}</div>
+      <img className="review-image" src={props.review.imageUrl}/>
+      <div className="review-body">{props.review.reviewBody}</div>
+      <button className="review-helpful">Is this review helpful?</button>
+
+    </li>
+  )
+}
+
+
+
+export default ReviewListEntry;
