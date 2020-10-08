ad33967ed96149a453ec8d165e80834693318f6d
Scott Mounce <scottmounce10@gmail.com>
2020-10-08 12:03:50 -0700

    finished up CSS with author, removed css tag from html and html template

--- server/template.js
+++ server/template.js
@@ -3,8 +3,7 @@ const component =
 `<!DOCTYPE html>
 <html lang='en'>
 
-  <head>
-    <link rel="stylesheet" href="./style.css"> 
+  <head>    
     <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@300&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Darker+Grotesque&display=swap" rel="stylesheet">
       
